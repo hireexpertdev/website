f21ff89528fde5e442994d2b2c30dc53d253014c
Sarthak <dabhi.sarthak@gmail.com>
2020-05-01 08:49:24 -0700

    chnage

---
+++ tailwind.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ["Source Sans Pro", "sans-serif"],
+      serif: ["Source Serif Pro", "serif"],
+    },
+    maxWidth: {
+      "960": "960px",
+    },
+    zIndex: {
+      "-10": "-10",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
