962a9a92664046621aff0f800ca9c9447410011e
Sarthak <dabhi.sarthak@gmail.com>
2020-05-02 01:46:19 -0700

    mix

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,58 @@
-if(!self.define){const e=e=>{"require"!==e&&(e+=".js");let r=Promise.resolve();return s[e]||(r=new Promise(async r=>{if("document"in self){const s=document.createElement("script");s.src=e,document.head.appendChild(s),s.onload=r}else importScripts(e),r()})),r.then(()=>{if(!s[e])throw new Error(`Module ${e} didn’t register its module`);return s[e]})},r=(r,s)=>{Promise.all(r.map(e)).then(e=>s(1===e.length?e[0]:e))},s={require:Promise.resolve(r)};self.define=(r,i,t)=>{s[r]||(s[r]=Promise.resolve().then(()=>{let s={};const o={uri:location.origin+r.slice(1)};return Promise.all(i.map(r=>{switch(r){case"exports":return s;case"module":return o;default:return e(r)}})).then(e=>{const r=t(...e);return s.default||(s.default=r),s})}))}}define("./service-worker.js",["./workbox-1bbb3e0e"],(function(e){"use strict";self.addEventListener("message",e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()}),e.precacheAndRoute([{url:"/dist/app.css",revision:"b4fbb467edf60a2c07dbe01c29fb79cd"},{url:"/mix.js",revision:"c94d8a9a873a3130d5778b9049580700"}],{})}));
+if (!self.define) {
+  const e = (e) => {
+      "require" !== e && (e += ".js");
+      let r = Promise.resolve();
+      return (
+        s[e] ||
+          (r = new Promise(async (r) => {
+            if ("document" in self) {
+              const s = document.createElement("script");
+              (s.src = e), document.head.appendChild(s), (s.onload = r);
+            } else importScripts(e), r();
+          })),
+        r.then(() => {
+          if (!s[e]) throw new Error(`Module ${e} didn’t register its module`);
+          return s[e];
+        })
+      );
+    },
+    r = (r, s) => {
+      Promise.all(r.map(e)).then((e) => s(1 === e.length ? e[0] : e));
+    },
+    s = { require: Promise.resolve(r) };
+  self.define = (r, i, t) => {
+    s[r] ||
+      (s[r] = Promise.resolve().then(() => {
+        let s = {};
+        const o = { uri: location.origin + r.slice(1) };
+        return Promise.all(
+          i.map((r) => {
+            switch (r) {
+              case "exports":
+                return s;
+              case "module":
+                return o;
+              default:
+                return e(r);
+            }
+          })
+        ).then((e) => {
+          const r = t(...e);
+          return s.default || (s.default = r), s;
+        });
+      }));
+  };
+}
+define("./service-worker.js", ["./workbox-1bbb3e0e"], function(e) {
+  "use strict";
+  self.addEventListener("message", (e) => {
+    e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting();
+  }),
+    e.precacheAndRoute(
+      [
+        { url: "/dist/app.css", revision: "b4fbb467edf60a2c07dbe01c29fb79cd" },
+        { url: "/index.html", revision: "c94d8a9a873a3130d5778b9049580700" },
+      ],
+      {}
+    );
+});
